f520b3222fae81c218b690c5293599c26dc7f5bb
Sudhanshu Barthwal <subarth@microsoft.com>
2018-04-16 10:21:36 +0530

    adding 1st iteration

---
+++ MyThreadPool/ThreadPool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyThreadPool
+{
+    public delegate void WaitCallBack(object state);
+
+    class ThreadPool
+    {
+        private int poolSize;
+
+        public ThreadPool(int poolSize)
+        {
+            this.poolSize = poolSize;
+        }
+
+        private int PoolSize => this.poolSize;
+
+        public bool QueueUserWorkItem(WaitCallBack waitCallBack, object param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPoolSize(int size)
+        {
+            this.poolSize = size;
+        }
+    }
+}
---
+++ MyThreadPool/ThreadPoolExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyThreadPool
+{
+    class ThreadPoolExample
+    {
+        private void RunThreadPoolExample()
+        {
+            ThreadPool thPool = new ThreadPool(5);
+
+            for(int i = 0; i < 100; i++)
+            {                
+                thPool.QueueUserWorkItem(new WaitCallBack(ThreadProc), i);
+            }
+        }
+
+        public void ThreadProc(object i)
+        {
+            Console.WriteLine("The value printed is:" + i);
+            Thread.Sleep(1000);
+        }
+    }
+}
