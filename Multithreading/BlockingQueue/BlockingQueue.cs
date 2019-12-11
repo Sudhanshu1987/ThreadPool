986b13088788f60eb4981d18c79dbfabb1ed868f
Sudhanshu Barthwal <subarth@microsoft.com>
2019-12-11 09:24:04 +0530

    building blocking queue

---
+++ Multithreading/BlockingQueue/BlockingQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multithreading.BlockingQueue
+{
+    class BlockingQueue
+    {
+        private int[] Queue;
+        private int MaxSize;
+        private int Head;
+        private int Tail;
+
+        public BlockingQueue(int capacity)
+        {
+            this.MaxSize = capacity;
+            this.Queue = new int[capacity];
+            this.Head = -1;
+            this.Tail = -1;
+        }
+
+        public bool Enqueue(int item)
+        {
+            return false;
+        }
+
+        public int Dequeue()
+        {
+            return -1;
+        }
+    }
+}
